781b67482f01f0be3faf2838314498524109fa7d
Rémy Raes <remy.raes@inria.fr>
2020-08-24 12:01:51 +0200

    [feat] localstorage allows the parser not to download countries at each rebuild

--- lib/utils/information/countrySelection/Covid19ApiParser.dart
+++ lib/utils/information/countrySelection/Covid19ApiParser.dart
@@ -1,10 +1,19 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:localstorage/localstorage.dart';
 import 'package:pandemia/utils/information/countrySelection/Country.dart';
 
 class Covid19ApiParser {
     List<Country> countries = new List();
+    final LocalStorage _storage = new LocalStorage('pandemia_app.json');
+    final String _countriesKey = 'countries';
+
+    Future<bool> hasDownloadedCountries () async {
+      await _storage.ready;
+      var countries = _storage.getItem(_countriesKey);
+      print(countries);
+    }
 
     Future<void> getCountries () async {
       String url = "https://api.covid19api.com/countries";
@@ -15,5 +24,8 @@ class Covid19ApiParser {
       for (var country in decoded) {
         countries.add( Country(name: country['Country'], identifier: country['ISO2']) );
       }
+
+      await _storage.ready;
+      _storage.setItem(_countriesKey, countries);
     }
 }
