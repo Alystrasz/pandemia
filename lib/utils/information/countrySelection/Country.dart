b3d3d94153bae44368ad64d388366b441ef5404f
Rémy Raes <remy.raes@inria.fr>
2020-08-24 15:38:04 +0200

    [feat] adding methods to Country for it to be serializable

--- lib/utils/information/countrySelection/Country.dart
+++ lib/utils/information/countrySelection/Country.dart
@@ -3,4 +3,18 @@ class Country {
   final String identifier;
 
   Country ({this.name, this.identifier});
+
+  Map toJson () => {
+    'name': name,
+    'identifier': identifier,
+  };
+
+  factory Country.fromJson(dynamic json) {
+    return Country(name: json['name'] as String,
+        identifier: json['identifier'] as String);
+  }
+
+  String toString () {
+    return 'Country {name: $name, identifier: $identifier}';
+  }
 }
