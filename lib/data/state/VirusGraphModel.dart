84ad9f6f7cd55164eecdb0c2f53b1a600a5c5963
Rémy Raes <remy.raes@inria.fr>
2020-09-01 09:59:07 +0200

    [feat] now storing selected province in model

--- lib/data/state/VirusGraphModel.dart
+++ lib/data/state/VirusGraphModel.dart
@@ -3,9 +3,15 @@ import 'package:flutter/cupertino.dart';
 /// Holds data used while drawing virus progression graph.
 class VirusGraphModel extends ChangeNotifier {
   String selectedCountry = "";
+  String province = "";
 
   setSelectedCountry (String value) {
     this.selectedCountry = value;
+    notifyListeners();
+  }
+
+  setProvince (String value) {
+    this.province = value;
     notifyListeners();
   }
 }
