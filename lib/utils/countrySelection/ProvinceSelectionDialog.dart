4cadd6377381c1ed544a451d8d51b49385640082
Rémy Raes <remy.raes@inria.fr>
2020-09-01 14:15:55 +0200

    [feat] styling ProvinceSelectionDialog with app colors

--- lib/utils/countrySelection/ProvinceSelectionDialog.dart
+++ lib/utils/countrySelection/ProvinceSelectionDialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:pandemia/data/state/VirusGraphModel.dart';
+import 'package:pandemia/utils/CustomPalette.dart';
 import 'package:pandemia/utils/countrySelection/CountrySelectionTile.dart';
 import 'package:provider/provider.dart';
 
@@ -12,9 +13,10 @@ class ProvinceSelectionDialog {
   ProvinceSelectionDialog (List<String> provinces) {
      provinces.sort((a, b) => a.compareTo(b));
     _dialog = SimpleDialog(
-      title: const Text('Choose a province'),
+      title: const Text('Choose a province', style: TextStyle(color: Color(0xFFDDDDDD))),
+      backgroundColor: CustomPalette.background[500],
       children: provinces.map((String p) => ListTile(
-        title: Text(p == '' ? "Home country" : p),
+        title: Text(p == '' ? "Home country" : p, style: TextStyle(color: CustomPalette.text[400])),
         onTap: () {
           Provider.of<VirusGraphModel>(_context).setProvince(p);
           _storage.setItem(CountrySelectionTile.selectedProvinceKey, p);
