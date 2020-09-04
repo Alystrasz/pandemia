72408d39eb80018315b5a5edbaf54ca5bd8cd42b
Rémy Raes <remy.raes@inria.fr>
2020-09-04 09:48:41 +0200

    [fix] IndicatorsCard no longer appears when loading data or when a country has no data

--- lib/components/virusAnalysis/IndicatorsCard.dart
+++ lib/components/virusAnalysis/IndicatorsCard.dart
@@ -7,61 +7,65 @@ import 'package:provider/provider.dart';
 class IndicatorsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container (
-      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-      child: Stack (
-        children: <Widget>[
-          Container(
-            color: CustomPalette.background[600],
-            width: 5000,
-            child: Stack (
-              children: <Widget>[
-                Container(
-                  child: new Text(
-                    'Indicators',
-                    style: TextStyle(
-                        color: CustomPalette.text[100],
-                        fontSize: 18,
-                        fontWeight: FontWeight.w300
+    return Consumer<VirusGraphModel>(
+        builder: (context, model, child) {
+          if (model.currentData == null || model.currentData.length == 0) {
+            return Container ();
+          } else {
+            return Container (
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+              child: Stack (
+                children: <Widget>[
+                  Container(
+                    color: CustomPalette.background[600],
+                    width: 5000,
+                    child: Stack (
+                      children: <Widget>[
+                        Container(
+                          child: new Text(
+                            'Indicators',
+                            style: TextStyle(
+                                color: CustomPalette.text[100],
+                                fontSize: 18,
+                                fontWeight: FontWeight.w300
+                            ),
+                          ),
+                          padding: EdgeInsets.all(10.0),
+                        ),
+
+                        Container(
+                            child: new Text(
+                              "Pandemic indicators for your region",
+                              style: TextStyle(
+                                  color: CustomPalette.text[600],
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w300
+                              ),
+                            ),
+                            padding: EdgeInsets.symmetric(vertical: 35.0, horizontal: 10.0)
+                        ),
+
+                        Container(
+                            margin: EdgeInsets.only(top: 70, left: 10),
+                            child: model.isParsingData ? Center (
+                              child: CircularProgressIndicator(),
+                            ) : new Text(
+                              "${model.currentData.last.activeCases} active cases now",
+                              style: TextStyle(
+                                color: CustomPalette.text[100],
+                                fontSize: 20,
+                              ),
+                            ),
+                            padding: EdgeInsets.only(bottom: 20)
+                        )
+                      ],
                     ),
                   ),
-                  padding: EdgeInsets.all(10.0),
-                ),
-
-                Container(
-                    child: new Text(
-                      "Pandemic indicators for your region",
-                      style: TextStyle(
-                          color: CustomPalette.text[600],
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300
-                      ),
-                    ),
-                    padding: EdgeInsets.symmetric(vertical: 35.0, horizontal: 10.0)
-                ),
-
-                Consumer<VirusGraphModel>(
-                  builder: (context, model, child) {
-                    return Container(
-                      margin: EdgeInsets.only(top: 70, left: 10),
-                        child: model.isParsingData ? Center (
-                          child: CircularProgressIndicator(),
-                        ) : new Text(
-                          "${model.currentData.last.activeCases} active cases now",
-                          style: TextStyle(
-                              color: CustomPalette.text[100],
-                              fontSize: 20,
-                          ),
-                        ),
-                        padding: EdgeInsets.only(bottom: 20)
-                    );
-                  }
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
+                ],
+              ),
+            );
+          }
+        }
     );
   }
 }
